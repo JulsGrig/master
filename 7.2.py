81825e1cd67ba8531cf4721023eb5130668fd2f7
uliagrigoreva <miss.u.yavorskaya@gmail.ru>
2023-05-19 16:51:54 +0300

    lab 7

--- 7.2.py
+++ 7.2.py
@@ -2,6 +2,7 @@ from PIL import Image
 filename = "a46b2bbcc6475ac0bd33e55e426da996-3.jpg"
 with Image.open(filename) as img:
     img.load()
+
 one_img= img.resize((img.width//3,img.height//3))
 one_img.save("new.jpg")
 one_img.show()
--- 7.4.py
+++ 7.4.py
@@ -3,6 +3,7 @@ water = "banana.png"
 
 with Image.open (water) as img_water:
    img_water.load()
+
 img_water = Image.open(water)
 img_water = img_water.resize((img_water.width // 10, img_water. height // 10))
 filename ="a46b2bbcc6475ac0bd33e55e426da996-3.jpg"
